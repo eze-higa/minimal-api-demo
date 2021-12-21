15b82514308d28e61a88974be06f152156009aa7
eze-higa <53649465+eze-higa@users.noreply.github.com>
2021-12-21 10:30:38 -0300

    Add DbContext and migrations

---
+++ src/minimal-api-demo/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using minimal_api_demo.Entities;
+
+namespace minimal_api_demo.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions options): base(options) { }
+        protected override void OnModelCreating(ModelBuilder builder) 
+        { 
+            builder.Entity<ToDoItem>().HasKey(x => x.Id);
+            builder.Entity<ToDoItem>().Property(x => x.Title).IsRequired();
+        }
+        public DbSet<ToDoItem> ToDoItems => Set<ToDoItem>();
+    }
+}
