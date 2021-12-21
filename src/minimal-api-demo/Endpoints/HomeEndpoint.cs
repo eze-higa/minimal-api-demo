b7992cb5f18c49a06e57248789795e666009c489
eze-higa <53649465+eze-higa@users.noreply.github.com>
2021-12-21 10:29:12 -0300

    Create HomeEndpoint.cs

---
+++ src/minimal-api-demo/Endpoints/HomeEndpoint.cs
@@ -0,0 +1,11 @@
+﻿namespace minimal_api_demo.Endpoints
+{
+    public static class HomeEndpoint
+    {
+        public static WebApplication AddHomeEndpoint(this WebApplication webApplication)
+        {
+            webApplication.MapGet("/api/", () => Results.Ok());
+            return webApplication;
+        }
+    }
+}
