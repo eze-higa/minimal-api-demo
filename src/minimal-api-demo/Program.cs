216a6d2d92402020c366c754e30fc3ad714cb6e0
eze-higa <53649465+eze-higa@users.noreply.github.com>
2021-12-21 11:07:46 -0300

    Update Program.cs
    Add basic endpoint

--- src/minimal-api-demo/Program.cs
+++ src/minimal-api-demo/Program.cs
@@ -1,6 +1,8 @@
 
+using Microsoft.EntityFrameworkCore;
 using minimal_api_demo.Data;
 using minimal_api_demo.Endpoints;
+using minimal_api_demo.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,8 +23,56 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapFallback(() => Results.Redirect("/swagger"));
 
 app.AddHomeEndpoint();
+
+app.MapGet("/todos", async (AppDbContext dbContext) => 
+{
+    return await dbContext.ToDoItems.ToListAsync();
+});
+
+app.MapGet("/todos/{id}", async (AppDbContext dbContext, int id) => 
+{ 
+    var todoItem = await dbContext.ToDoItems.FindAsync(id);
+    if(todoItem != null)
+        return Results.Ok(todoItem);
+
+    return Results.NotFound();
+});
+
+app.MapPost("/todos", async (AppDbContext dbContext, ToDoItem toDo) =>
+{ 
+    await dbContext.ToDoItems.AddAsync(toDo);
+    await dbContext.SaveChangesAsync();
+
+    return Results.Created($"/todos/{toDo.Id}", toDo);
+});
+
+app.MapPut("/todos/{id}", async (AppDbContext dbContext, int id, ToDoItem toDo) => 
+{
+    if(id != toDo.Id)
+        return Results.BadRequest();
+
+    if (!await dbContext.ToDoItems.AnyAsync(t => t.Id == id))
+        return Results.NotFound();
+
+    dbContext.ToDoItems.Update(toDo);
+    await dbContext.SaveChangesAsync();
+    return Results.Ok();
+});
+
+app.MapDelete("/todos/id", async (AppDbContext dbContext, int id) => 
+{
+    var todoItem = await dbContext.ToDoItems.FindAsync(id);
+    
+    if(todoItem == null)
+        return Results.NotFound();
+
+    dbContext.ToDoItems.Remove(todoItem);
+    await dbContext.SaveChangesAsync();
+    return Results.Ok();
+});
 
 var port = Environment.GetEnvironmentVariable("PORT") ?? "3000";
 
