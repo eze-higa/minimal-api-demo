7596958c22a748b543ea9827e41c13aab9015b6b
eze-higa <53649465+eze-higa@users.noreply.github.com>
2021-12-21 10:30:18 -0300

    Create ToDoItem.cs
    Add todo entity

---
+++ src/minimal-api-demo/Entities/ToDoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace minimal_api_demo.Entities
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public bool IsComplete { get; set; }        
+
+    }
+}
